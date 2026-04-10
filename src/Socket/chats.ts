02c24a4b97c30e31dad247a58cc840fdd15c780d
Renato Freitas <renatoiub@live.com>
2026-04-10 02:40:31 -0300

    update abprops query to fix bad-request error on startUp
    By vinikjkkj, WhiskeySockets

--- src/Socket/chats.ts
+++ src/Socket/chats.ts
@@ -761,20 +761,20 @@ export const makeChatsSocket = (config: SocketConfig) => {
 	}
 
 	/** sending non-abt props may fix QR scan fail if server expects */
-	const fetchProps = async () => {
+		const fetchProps = async () => {
 		const resultNode = await query({
 			tag: 'iq',
 			attrs: {
 				to: S_WHATSAPP_NET,
-				xmlns: 'w',
+				xmlns: 'abt',
 				type: 'get'
 			},
 			content: [
 				{
 					tag: 'props',
 					attrs: {
-						protocol: '2',
-						hash: authState?.creds?.lastPropHash || ''
+						protocol: '1',
+						...(authState?.creds?.lastPropHash ? { hash: authState.creds.lastPropHash } : {})
 					}
 				}
 			]
@@ -797,6 +797,7 @@ export const makeChatsSocket = (config: SocketConfig) => {
 
 		return props
 	}
+
 
 	/**
 	 * modify a chat -- mark unread, read etc.
