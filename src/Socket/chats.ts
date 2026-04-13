81f10a8a7d29f20163ed7a003bf7f0c665562585
Zapitu Project Manager <libzapitu+main@gmail.com>
2026-04-13 13:51:24 +0000

    port from https://github.com/WhiskeySockets/Baileys/pull/2473

--- src/Socket/chats.ts
+++ src/Socket/chats.ts
@@ -761,21 +761,21 @@ export const makeChatsSocket = (config: SocketConfig) => {
 		}
 	}
 
-	/** sending non-abt props may fix QR scan fail if server expects */
-	const fetchProps = async () => {
+	/** fetch AB props */
+		const fetchProps = async () => {
 		const resultNode = await query({
 			tag: 'iq',
 			attrs: {
 				to: S_WHATSAPP_NET,
-				xmlns: 'w',
+				xmlns: 'abt',
 				type: 'get'
 			},
 			content: [
 				{
 					tag: 'props',
 					attrs: {
-						protocol: '2',
-						hash: authState?.creds?.lastPropHash || ''
+						protocol: '1',
+						...(authState?.creds?.lastPropHash ? { hash: authState.creds.lastPropHash } : {})
 					}
 				}
 			]
